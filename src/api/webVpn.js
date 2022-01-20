26fa93a8d1a6ffc3dc99a71a3acae3661fdb73ad
gaowanlu <2209120827@qq.com>
2022-01-20 18:30:54 +0800

    text middleware

--- src/api/webVpn.js
+++ src/api/webVpn.js
@@ -1,13 +1,17 @@
 import axios from "axios"
 
 const webVpn = async (username, password, callback) => {
-    let rp = await axios.post('http://61.171.51.135:5557/', {
+    let rp = await axios.post('/backend/', {
         username,
         password
     }, {
         timeout: 120000
+    }).catch((e) => {
+        /*将最新内容存储。。。*/
+        callback({
+            status: 500
+        });
     });
-    /*将最新内容存储。。。*/
     callback(rp);
 }
 

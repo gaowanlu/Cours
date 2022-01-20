4ffd326fac046b0d0e87dae5bd4f6069cc4896ef
gaowanlu <2209120827@qq.com>
2022-01-20 19:15:35 +0800

    更改代理超时时间

--- api/proxy.js
+++ api/proxy.js
@@ -15,7 +15,7 @@ module.exports = (req, res) => {
     createProxyMiddleware({
         target,
         changeOrigin: true,
-        timeout: 12000,
+        timeout: 120000,
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/backend`
             // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login

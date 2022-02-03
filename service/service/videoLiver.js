e8d86c60fac581cd70dd965c4fbf51ed15d43e47
gaowanlu <2209120827@qq.com>
2022-02-03 22:47:33 +0800

    ffpmeg直播测试

--- service/service/videoLiver.js
+++ service/service/videoLiver.js
@@ -1,9 +1,12 @@
 const NodeMediaServer = require('node-media-server');
-
+// use ffmpeg
+// ./ffmpeg -f dshow -i video="Integrated Webcam" -vcodec h264 -acodec copy -r 30 -offset_x 10 -offset_y 20  -f flv rtmp://127.0.0.1:1935/live/cliver
+//ffmpeg -f dshow -i video="Integrated Webcam" -f dshow -i audio="麦克风阵列 (英特尔® 智音技术)" -vcodec h264 -acodec copy -r 30 -offset_x 10 -offset_y 20  -f flv rtmp://127.0.0.1:1935/live/cliver
+// ./ffmpeg -f dshow -i video="Integrated Webcam" -f dshow -i audio="麦克风阵列 (英特尔® 智音技术)" -vcodec h264 -preset:v ultrafast -pix_fmt yuv420p -acodec aac -f flv rtmp://127.0.0.1:1935/live/cliver
 const config = {
     rtmp: {
         port: 1935,
-        chunk_size: 60000,
+        chunk_size: 20000,
         gop_cache: true,
         ping: 30,
         ping_timeout: 6000,

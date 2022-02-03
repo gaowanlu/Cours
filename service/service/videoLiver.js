d9bf3a8c9368cab122439067c590aacd29f2997a
gaowanlu <2209120827@qq.com>
2022-02-03 23:43:05 +0800

    SSL

--- service/service/videoLiver.js
+++ service/service/videoLiver.js
@@ -2,7 +2,7 @@ const NodeMediaServer = require('node-media-server');
 const fs = require("fs");
 // use ffmpeg
 // ./ffmpeg -f dshow -i video="Integrated Webcam" -vcodec h264 -acodec copy -r 30 -offset_x 10 -offset_y 20  -f flv rtmp://127.0.0.1:1935/live/cliver
-//ffmpeg -f dshow -i video="Integrated Webcam" -f dshow -i audio="麦克风阵列 (英特尔® 智音技术)" -vcodec h264 -acodec copy -r 30 -offset_x 10 -offset_y 20  -f flv rtmp://127.0.0.1:1935/live/cliver
+// ./ffmpeg -f dshow -i video="Integrated Webcam" -f dshow -i audio="麦克风阵列 (英特尔® 智音技术)" -vcodec h264 -acodec copy -r 30 -offset_x 10 -offset_y 20  -f flv rtmp://127.0.0.1:1935/live/cliver
 // ./ffmpeg -f dshow -i video="Integrated Webcam" -f dshow -i audio="麦克风阵列 (英特尔® 智音技术)" -vcodec h264 -preset:v ultrafast -pix_fmt yuv420p -acodec aac -f flv rtmp://127.0.0.1:1935/live/cliver
 const config = {
     rtmp: {

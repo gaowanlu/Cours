c6635845127ff89338fdc94d57968919fe3c54c4
gaowanlu <2209120827@qq.com>
2022-02-20 23:24:59 +0800

    项目分类整理

--- assembly/build.sh
+++ assembly/build.sh
@@ -2,7 +2,7 @@ rm -rf ../public/assembly/* & \
 emcc \
 main.cpp \
 -s WASM=1 \
--o ../public/assembly/main.js \
+-o ../cours-react/public/assembly/main.js \
 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']" 
 
 echo "=>BUILD END";
--- src/api/config.js
+++
@@ -1,8 +0,0 @@
-const host = "linkway.site";
-const webVpnPath = `https://${host}:5557/`;
-const talkSocketPath = `https://${host}:5558`;
-
-export {
-    webVpnPath,
-    talkSocketPath
-};

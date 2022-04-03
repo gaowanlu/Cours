bdc986e0e5c37aa3a5fa6a8ba89c2a16f3b6f05e
gaowanlu <2209120827@qq.com>
2022-04-03 17:42:11 +0800

    cours-react 添加访问记录提交

--- cours-react/src/data/welcome.ts
+++ cours-react/src/data/welcome.ts
@@ -1,10 +1,18 @@
 import baseInfo from './baseInfo.json';
+import registerAPI from '../api/register.js';
 
+/*
+* 做一些用户进入页面然后此时我们想做的事情
+*/
 function welcome() {
     let version: string = baseInfo.version;
     console.info(draw);
     console.log("我们携起手来改变世界 2022 @桂林电子科技大学");
     console.log("Cours " + version);
+    //像平台提交访问记录
+    register.postUsername((res)=>{
+        console.log("Welcome");
+    },String(Math.random()))
 }
 
 const draw: string =`

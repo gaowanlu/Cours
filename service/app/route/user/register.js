6658be81324b67ccd0a311be89ff958b2b347571
gaowanlu <2209120827@qq.com>
2022-04-03 18:01:10 +0800

    service::添加运行时访问量记录

--- service/app/route/user/register.js
+++ service/app/route/user/register.js
@@ -1,16 +1,27 @@
 const requestJSON=require("../../../utils/requestJson");
 
+const counter={
+    sum:0,
+    add:function (){
+        this.sum++;
+    }
+}
+
 /*
  *平台访问情况记录
  * */
 async function register(req,res){
+    res.setHeader('Content-Type','application/json;utf-8');
     let json=await requestJSON.jsonReceiver(req);
     if(json!==null){
         if(json.username&&typeof(json.username)==='string'){
-            //插入到DB
             console.log(json.username,new Date());
+            counter.add();
         }
     }
+    res.write(JSON.stringify({
+        sum:counter.sum
+    }));
     res.end();
 }
 module.exports=register;
